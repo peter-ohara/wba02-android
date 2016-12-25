8212cf2e17af754a67e1e425ac4343e9c1351e37
Peter O'Hara Adu <ohara.invent@gmail.com>
2016-12-25 13:47:07 +0000

    Delete unused files in the assets folder
    This commit deletes unused files in the assets folder. It also
    removes unused code from the rest of the files left in the assets
    folder.

--- app/src/main/assets/js/custom-script.js
+++ app/src/main/assets/js/custom-script.js
@@ -1,35 +1,7 @@
-
-
-$(document).ready(function(){
-  $("#checkButton").click(function(){
-    $(".progress").show(600);
-    $(".percentage").addClass("showPercentage");
-  });
-
-  /*
-  $('#discussionButton').click(function(){
-  if ( $('#pasco-comments').css('visibility') == 'hidden' )
-    $('#pasco-comments').css('visibility','visible');
-  else
-    $('#video-over').css('visibility','hidden');
-  });
-  $('#comments-container').comments({
-    sendText: 'Comment'
-  });
-
-  $( "#book" ).slideUp( "slow", function() {
-    // Animation complete.
-  });
-  */
-
-  $("#discussionButton").click(function(){
-    $("#pasco-comments").slideToggle("fast", function(){
-      $("pasco-comments").slideDown("slow");
-  });
-    
-});
-
-
-
-  
+$(document).ready(function() {
+    $("#discussionButton").click(function(){
+        $("#pasco-comments").slideToggle("fast", function(){
+            $("pasco-comments").slideDown("slow");
+        });
+    });
 });
--- app/src/main/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-  $("#checkButton").click(function(){
-    $(".progress").show(600);
-    $(".percentage").addClass("showPercentage");
-    });
-});
